081d297ac13ead1351636a59d2b218c95a0bc18c
Burhan Farah Hassan <bhassan2@byupathway.edu>
2026-04-02 13:32:32 +0300

    FINISHED W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Burhan Farah Hassan", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine(); // blank line for separation
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
